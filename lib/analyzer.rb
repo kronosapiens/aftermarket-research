ad3965e1e7ce6578de96ec608a203ccc7fb9a32f
Daniel Kronovet <kronovet@gmail.com>
2014-03-23 15:27:07 -0400

    initial work implementing autotune in analyzer

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -1,6 +1,6 @@
 class Analyzer
 
-  attr_accessor :num_array
+  attr_accessor :num_array, :new_min, :new_max
 
   def initialize(num_array)
     self.num_array = num_array
@@ -20,6 +20,28 @@ class Analyzer
   def mode
     freq = self.num_array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
     self.num_array.max_by { |v| freq[v] }
+  end
+
+  def auto_tune
+    benchmark = mean / 2.0
+
+    if num_array.length <= 10
+      self.new_min = false
+      self.new_max = false
+      "AutoTune cannot adjust for less than 10 results."
+    elsif median < benchmark
+      self.new_min = false 
+      self.new_max = mean 
+      "AutoTune analysis complete -- high mean, new maximum set."
+    elsif mode < benchmark
+      self.new_min = benchmark
+      self.new_max = false
+      "AutoTune analysis complete -- low mode, new minimum set."
+    else
+      self.new_min = false
+      self.new_max = false
+      "AutoTune analysis complete -- your search is good."
+    end
   end
 
   def print_calculations
--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -2,7 +2,10 @@ require_relative 'spec_helper.rb'
 
 describe "Analyzer" do
 
-let(:analyzer){Analyzer.new([1, 1, 1, 2, 4, 5, 7])}
+let(:normal_nums){[1, 1, 1, 2, 4, 5, 7]}
+let(:low_mode){[1, 1, 1, 1, 4, 6, 8, 8, 9, 9, 10]}
+let(:high_mean){[1, 1, 2, 2, 3, 4, 4, 4, 5, 7, 8, 8, 9, 10, 11, 100]}
+let(:analyzer){Analyzer.new(normal_nums)}
 
   it 'calculates the mean' do
     mean = analyzer.mean
@@ -17,6 +20,32 @@ let(:analyzer){Analyzer.new([1, 1, 1, 2, 4, 5, 7])}
   it 'calculates the mode' do
     mode = analyzer.mode
     expect(mode).to be(1)
+  end
+
+  it 'adjusts for a low mode' do
+    analyzer = Analyzer.new(low_mode)
+    analyzer.auto_tune
+    expect(analyzer.new_max).to be(false)
+    expect(analyzer.new_min).to eq(analyzer.mean / 2.0)
+  end
+
+  it 'adjusts for a high mean' do
+    analyzer = Analyzer.new(high_mean)
+    analyzer.auto_tune
+    expect(analyzer.new_min).to be(false)
+    expect(analyzer.new_max).to eq(analyzer.mean)
+  end
+
+  it 'sets new_min to false if the search is good' do
+    analyzer.auto_tune
+    expect(analyzer.new_max).to be(false)
+    expect(analyzer.new_min).to be(false)
+  end
+
+  it 'returns false without enough data' do
+    analyzer.auto_tune
+    expect(analyzer.new_max).to be(false)
+    expect(analyzer.new_min).to be(false)
   end
 
 end
